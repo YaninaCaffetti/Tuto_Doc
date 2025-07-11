2f44bfca2366ab35d46e0e48a6dbb12fd1ec26cf
Yanina Andrea Caffetti <43344231+YaninaCaffetti@users.noreply.github.com>
2025-07-11 09:07:10 -0300

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,2 @@
 # src/__init__.py
-
-# Este archivo hace que la carpeta 'src' sea un paquete de Python,
-# permitiendo la importación de sus módulos.
+# Este archivo hace que la carpeta 'src' sea un paquete de Python.
