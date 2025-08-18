013d8b1c8c89ce2d945c72c4e7e50d4adcd20d9d
Yanina Andrea Caffetti <43344231+YaninaCaffetti@users.noreply.github.com>
2025-08-18 14:16:43 -0300

    Update emotion_classifier.py

--- src/emotion_classifier.py
+++ src/emotion_classifier.py
@@ -210,12 +210,12 @@ class WeightedLossTrainer(Trainer):
         if self.class_weights is not None:
             print(f"  › Pesos de clase: {np.round(self.class_weights.detach().cpu().numpy(), 3)}")
 
-    def compute_loss(self, model, inputs, return_outputs=False):
+    def compute_loss(self, model, inputs, return_outputs=False, **kwargs): 
         labels = inputs.pop("labels")
         outputs = model(**inputs)
         logits = outputs.get("logits")
         loss_fct = nn.CrossEntropyLoss(weight=self.class_weights)
-        loss = loss_fct(logits.view(-1, self.model.config.num_labels), labels.view(-1))
+        loss = loss_fct(logits.view(-1, self.model.config.num_labels), labels.view(--1))
         return (loss, outputs) if return_outputs else loss
 
 
